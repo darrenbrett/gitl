03302a192c1607183c90894cb36dbd8d6498ad32
Darren King <darrenbrettking@hotmail.com>
2016-10-16 14:55:17 -0700

    Added model and started provider service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 import { Users } from '../pages/users/users';
 import { Repos } from '../pages/repos/repos';
 import { Orgs } from '../pages/orgs/orgs';
-
 
 @Component({
   templateUrl: 'app.html'
@@ -24,8 +21,6 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 },
       { title: 'Users', component: Users },
       { title: 'Repos', component: Repos },
       { title: 'Orgs', component: Orgs }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
 import { Users } from '../pages/users/users';
 import { Repos } from '../pages/repos/repos';
 import { Orgs } from '../pages/orgs/orgs';
@@ -10,8 +8,6 @@ import { Orgs } from '../pages/orgs/orgs';
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2,
     Users,
     Repos,
     Orgs
@@ -22,8 +18,6 @@ import { Orgs } from '../pages/orgs/orgs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2,
     Users,
     Repos,
     Orgs
