bd26a6e994b2cc8301c9d7393952a0a473ddf100
Darren King <darrenbrettking@hotmail.com>
2016-10-16 21:33:51 -0700

    Fixed errors and added search

--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { User } from '../../models/user';
+
 import { GithubUsers } from '../../providers/github-users';
+
 import { UserDetails } from '../user-details/user-details';
 
 @Component({
@@ -11,14 +13,30 @@ import { UserDetails } from '../user-details/user-details';
 })
 export class Users {
   users: User[]
+  originalUsers: User[];
 
   constructor(public navCtrl: NavController, private githubUsers: GithubUsers) {
     githubUsers.load().subscribe(users => {
       this.users = users;
+      this.originalUsers = users;
     })
+  }
 
-    goToDetails(login: string) {
-      this.navCtrl.push(UserDetails, {login});
+  goToDetails(login: string) {
+    this.navCtrl.push(UserDetails, {login});
+  }
+
+  search(searchEvent) {
+    let term = searchEvent.target.value
+    // We will only perform the search if we have 3 or more characters
+    if (term.trim() === '' || term.trim().length < 3) {
+      // Load cached users
+      this.users = this.originalUsers;
+    } else {
+      // Get the searched users from github
+      this.githubUsers.searchUsers(term).subscribe(users => {
+        this.users = users
+      });
+    }
   }
 }
-
